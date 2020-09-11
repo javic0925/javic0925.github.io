1498ed5437c816c083b2aac4c27b842d50802a88
Javier Cruz <javic0925@gmail.com>
2020-09-11 12:00:07 -0400

    Delete app.js

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("./sw.js")
-    .then((reg) => console.log("service worker registered"))
-    .catch((err) => console.log("Service worker not registered", err));
-}
